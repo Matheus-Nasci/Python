8f530928126137c4b882b96822a36334b95638a4
Matheus-Nasci <matheus.nasci2002@hotmail.com>
2022-10-24 16:00:53 -0300

    listas, tupla e for

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,37 @@ def jogar():
     print("Bem-vindo ao jogo da Forca!")
     print("***************************")
 
-if(__name__ == "__main__"):
-    jogar()
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
+    enforcou = False
+    acertou = False
+    tentativas = 0
+
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
+        print(tentativas)
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (letra == chute):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas += 1
+
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
+        if (acertou):
+            print(letras_acertadas)
+            print("Parabéns, você ganhou o jogo")
+
+
+print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
